89d14100e7231396b82745eefc3be4f0151f02b2
unknown <karsont18@gmail.com>
2024-01-31 20:56:13 -0700

    Better system, finished.

--- Program.cs
+++ Program.cs
@@ -1,60 +1,55 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-const System.ConsoleColor c_Cyan = ConsoleColor.Cyan;
-const System.ConsoleColor c_Red = ConsoleColor.Red;
-const System.ConsoleColor c_Blue = ConsoleColor.Blue;
-const System.ConsoleColor c_Yellow = ConsoleColor.Yellow;
-const System.ConsoleColor c_Brown = ConsoleColor.DarkYellow;
-const System.ConsoleColor c_White = ConsoleColor.White;
-const System.ConsoleColor c_Black = ConsoleColor.Black;
-
-void Cyan(){
-    Console.BackgroundColor = ConsoleColor.Cyan;
-    Console.Write("  ");
-}
-void Red(){
-    Console.BackgroundColor = ConsoleColor.Red;
-    Console.Write("  ");
-}
-void Blue(){
-    Console.BackgroundColor = ConsoleColor.Blue;
-    Console.Write("  ");
-}
-void Yellow(){
-    Console.BackgroundColor = ConsoleColor.Yellow;
-    Console.Write("  ");
-}
-void Brown(){
-    Console.BackgroundColor = ConsoleColor.DarkYellow;
-    Console.Write("  ");
-}
-void White(){ // No good color built in for the skin
-    Console.BackgroundColor = ConsoleColor.White;
-    Console.Write("  ");
-}
-void Black(){
-    Console.BackgroundColor = ConsoleColor.Black;
-    Console.Write("  ");
-}
-void Draw(int pixels, System.ConsoleColor color){
-    Console.BackgroundColor = color;
-    for(int i=0;i<pixels;i++){
-        Console.Write("  ");
-    } 
-    Console.ResetColor();
-}
-void DrawNl(int pixels, System.ConsoleColor color){ // Apparently you cannot overload local functions
-    Console.BackgroundColor = color;
-    for(int i=0;i<pixels;i++){
-        Console.Write("  ");
-    } 
-    Console.ResetColor();
-    Console.Write('\n');
-}
+﻿// 0=cyan, 1=red, 2=white, 3=black, 4=yellow, 5=brown, 6=blue
+int[] MarioArray = new int[] 
+{
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,1,1,1,1,1,0,0,0,0,0,
+    0,0,0,1,1,1,1,1,1,1,1,1,0,0,
+    0,0,0,3,3,3,2,2,3,2,0,0,0,0,
+    0,0,3,2,3,2,2,2,3,2,2,2,0,0,
+    0,0,3,2,3,3,2,2,2,3,2,2,2,0,
+    0,0,3,3,2,2,2,2,3,3,3,3,0,0,
+    0,0,0,0,2,2,2,2,2,2,2,0,0,0,
+    0,0,0,6,6,1,6,6,6,0,0,0,0,0,
+    0,0,6,6,6,1,6,6,1,6,6,6,0,0,
+    0,6,6,6,6,1,6,6,1,6,6,6,6,0,
+    0,2,2,6,6,1,1,1,1,6,6,2,2,0,
+    0,2,2,2,1,4,1,1,4,1,2,2,2,0,
+    0,2,2,1,1,1,1,1,1,1,1,2,2,0,
+    0,0,0,1,1,1,0,0,1,1,1,0,0,0,
+    0,0,5,5,5,0,0,0,5,5,5,0,0,0,
+    0,5,5,5,5,0,0,0,5,5,5,5,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0
+};
 
 Console.Clear();
 Console.SetCursorPosition(0, 0);
-DrawNl(14, c_Cyan);
-Draw(4, c_Cyan); Draw(5, c_Red); DrawNl(5, c_Cyan);
-Draw(3, c_Cyan); Draw(9, c_Red); DrawNl(2, c_Cyan);
-Draw(3, c_Cyan); Draw(3, c_Black); Draw(2, c_White); Black(); White(); DrawNl(4, c_Cyan);
+
+for(int i=0;i<MarioArray.Length;i++){
+    switch(MarioArray[i]){
+        case 0: Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.Write("  ");
+                break;
+        case 1: Console.BackgroundColor = ConsoleColor.Red;
+                Console.Write("  ");
+                break;
+        case 2: Console.BackgroundColor = ConsoleColor.White; // There was no good color for skin
+                Console.Write("  ");
+                break;
+        case 3: Console.BackgroundColor = ConsoleColor.Black;
+                Console.Write("  ");
+                break;
+        case 4: Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.Write("  ");
+                break;
+        case 5: Console.BackgroundColor = ConsoleColor.DarkYellow;
+                Console.Write("  ");
+                break;
+        case 6: Console.BackgroundColor = ConsoleColor.Blue;
+                Console.Write("  ");
+                break;
+        default: Console.WriteLine("INVALID"); break;
+
+    }
+    if(i !=0 && (i + 1) % 14 == 0){Console.ResetColor(); Console.Write('\n');}
+}
+Console.ResetColor();
--- obj/Debug/net8.0/lab-1-5-console-commands-UrRocklicker.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-1-5-console-commands-UrRocklicker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-1-5-console-commands-UrRocklicker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2069ee07f9e8313cdb31a42c8d0d7ef227e9627d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+098b0f11f754bace6bf075887ef0b12914bef89e")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-1-5-console-commands-UrRocklicker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-1-5-console-commands-UrRocklicker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
